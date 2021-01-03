b6936aa7af5b2483819062786855c31b6aca646e
assassin_box <assassin543@outlook.jp>
2021-01-03 18:10:04 +0900

    kye削除

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -3,7 +3,7 @@ import './Todoitem.css';
 
 const TodoItem=({todo, onClick})=>{
   return(
-    <div className='todo-item' key={todo.id} onClick={onClick}>
+    <div className='todo-item' onClick={onClick}>
       <div className='todo-item-title'>{todo.title}</div>
       <div className='todo-item-description'>{todo.description}</div>
     </div>
